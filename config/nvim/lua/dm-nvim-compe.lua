3efbd639239dd0537e9f53b9a942663d0bac2e10
Devin Morgenstern <devin.morgenstern@protonmail.com>
2021-05-10 11:17:41 -0700

    remove snippet support

--- config/nvim/lua/dm-nvim-compe.lua
+++ config/nvim/lua/dm-nvim-compe.lua
@@ -20,7 +20,6 @@ require'compe'.setup {
     calc = true;
     nvim_lsp = true;
     nvim_lua = true;
-    vsnip = true;
   };
 }
 
@@ -39,12 +38,9 @@ end
 
 -- Use (s-)tab to:
 --- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
   elseif check_back_space() then
     return t "<Tab>"
   else
@@ -54,8 +50,6 @@ end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
   else
     return t "<S-Tab>"
   end
