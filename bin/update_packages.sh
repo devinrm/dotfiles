42dfc1f25cb6608d782a76d77cef85653110f71b
Devin Morgenstern <devin.morgenstern@protonmail.com>
2021-05-29 20:34:04 -0700

    add more default python packages

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -3,6 +3,8 @@
 set -e
 
 brew update && brew upgrade && brew cleanup ;
-pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U ;
+pip install --upgrade pip
+npm i -g npm
 npm outdated -g ;
 npm update -g ;
