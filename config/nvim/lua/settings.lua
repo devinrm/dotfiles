47d0787db4cbdeb2697671e8709e7dd3686b1026
Devin Morgenstern <devin.morgenstern@protonmail.com>
2021-08-04 15:47:35 -0700

    remove settings that are already covered by nvim defaults

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -5,48 +5,35 @@
 
 vim.bo.smartindent = true
 vim.cmd('colorscheme ' .. 'nord')
-vim.cmd('filetype plugin indent on') -- load indent file for language
 vim.cmd('filetype plugin on')
-vim.cmd('runtime! macros/matchit.vim') -- Extended matching with '%'. See :help matchit
 vim.cmd('set spellfile=$HOME//.vim-spell-en.utf-8.add') -- Name of the word list file where words are added for the |zg| and |zw| commands.
-vim.cmd('set undodir=$HOME//.undodir') -- directory name for undo file.
-vim.cmd('syntax on') -- Turn on syntax highlighting. This must come before statusline
 vim.g.nord_cursorline_transparent = true
 vim.g.nord_borders = true
 vim.g.is_posix = 1 -- When the type of shell script is /bin/sh, assume a POSIX-compatible shell for syntax highlighting purposes.
 vim.g.mapleader = ' ' -- Set Leader key to <Space> bar
-vim.o.autoread = true
 vim.o.autowriteall = true
-vim.o.background = 'dark' -- Use colors that look good on a dark background
 vim.o.backspace = '2' -- Backspace deletes like most programs in insert mode
 vim.o.backup = false -- Don't make a backup before overwriting a file
 vim.o.backupcopy = 'yes'
 vim.o.clipboard = 'unnamedplus' -- copy paste to system clipboard
 vim.o.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>.
-vim.o.fileencoding = 'utf-8' -- The encoding written to file
 vim.o.foldenable = false -- Leave open all folds
 vim.o.formatprg = 'fmt'
 vim.o.gdefault = true -- Replace all matches on a line instead of just the first
 vim.o.grepformat = '%f:%l:%c:%m,%f:%l:%m'
 vim.o.grepprg = 'rg --vimgrep --no-heading'
-vim.o.history = 50 -- remember the last 50 command-lines in the history table
-vim.o.hlsearch = true -- highlight search results
 vim.o.ignorecase = true -- case insensitive pattern matching
 vim.o.inccommand = 'split' -- this is necessary for using this %s with a quickfix window in nvim
-vim.o.incsearch = true -- do incremental searching
 vim.o.joinspaces = false -- Insert one space after a '.', '?' and '!' with a join command.
-vim.o.laststatus = 2
 vim.o.lazyredraw = true
 vim.o.matchtime = 0 -- Speed up escape after (){} chars
 vim.o.mouse = 'a' -- Enable mouse for pair programming
 vim.o.pumheight = 10 -- limit size of popup menu
 vim.o.pumblend = 20
 vim.o.redrawtime = 1000 -- Stop highlighting if it takes more than a second
-vim.o.ruler = true -- show the cursor position all the time
 vim.o.scrolloff = 3 -- show 3 lines above and below cursor
 vim.o.shiftround = true -- Round indent to multiple of 'shiftwidth'.
 vim.o.shiftwidth = 2 -- Returns the effective value of 'shiftwidth'
-vim.o.showcmd = true -- display incomplete commands
 vim.o.showmode = false -- If in Insert, Replace or Visual mode don't put a message on the last line.
 vim.o.showtabline = 2
 vim.o.smartcase = true -- overrides ignorecase if pattern contains upcase
@@ -65,16 +52,13 @@ vim.o.undolevels = 500
 vim.o.undoreload = 500
 vim.o.updatetime = 100
 vim.o.wildignore = 'tmp/**' -- Ignore stuff that can't be opened
-vim.o.wildmenu = true -- Enables a menu at the bottom of the vim window.
 vim.o.wildmode = 'list:longest,list:full'
-vim.o.wildoptions = 'pum'
 vim.o.writebackup = false -- Don't make a backup before overwriting a file.
 vim.opt.colorcolumn = '+1' -- highlight column after 'textwidth
 vim.opt.completeopt:remove({'i', 't', 'preview'})
 vim.opt.diffopt:append({'vertical'}) -- Start diff mode with vertical splits
 vim.opt.guicursor:append({'a:blinkon0'}) -- Disable blinking cursor on nvim
 vim.opt.list = true
-vim.opt.listchars = { tab = '»·', trail = '·', nbsp = '·' } -- Display extra whitespace
 vim.opt.path:append({'.,,', '.,app/javascript', 'node_modules'})
 vim.opt.shortmess = 'filnxtToOFc' -- vim default with 'c' appended (don't give |ins-completion-menu| messages)
 vim.wo.cursorline = true
