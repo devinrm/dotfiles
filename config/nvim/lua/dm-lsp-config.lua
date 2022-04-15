9ec0c82d5537e61aabc25a166b1057996284288e
Devin Morgenstern <devin.morgenstern@protonmail.com>
2022-04-15 01:12:19 -0700

    daemons aren't setup in our docker

--- config/nvim/lua/dm-lsp-config.lua
+++ config/nvim/lua/dm-lsp-config.lua
@@ -59,10 +59,10 @@ lsp.jsonls.setup(coq.lsp_ensure_capabilities{
 })
 
 local null_ls_sources = {
-  null_ls.builtins.code_actions.eslint_d,
-  null_ls.builtins.diagnostics.eslint_d,
-  null_ls.builtins.formatting.eslint_d,
-  null_ls.builtins.formatting.prettierd,
+  null_ls.builtins.code_actions.eslint,
+  null_ls.builtins.diagnostics.eslint,
+  null_ls.builtins.formatting.eslint,
+  null_ls.builtins.formatting.prettier,
   null_ls.builtins.code_actions.refactoring,
   null_ls.builtins.diagnostics.rubocop,
   null_ls.builtins.formatting.rubocop,
