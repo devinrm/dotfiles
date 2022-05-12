68ad4db3c9e57acdcc54d94dfeb195a30a83c567
Devin Morgenstern <devin.morgenstern@protonmail.com>
2022-05-11 19:13:27 -0700

    Add more linters

--- config/nvim/lua/dm-lsp-config.lua
+++ config/nvim/lua/dm-lsp-config.lua
@@ -62,6 +62,9 @@ local null_ls_sources = {
   null_ls.builtins.code_actions.eslint,
   null_ls.builtins.diagnostics.eslint,
   null_ls.builtins.formatting.eslint,
+  null_ls.builtins.diagnostics.hadolint,
+  null_ls.builtins.diagnostics.haml_lint,
+  null_ls.builtins.diagnostics.jsonlint,
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.code_actions.refactoring,
   null_ls.builtins.diagnostics.rubocop,
@@ -70,6 +73,7 @@ local null_ls_sources = {
   null_ls.builtins.diagnostics.shellcheck,
   null_ls.builtins.diagnostics.stylelint,
   null_ls.builtins.formatting.stylelint,
-  null_ls.builtins.diagnostics.yamllint
+  null_ls.builtins.diagnostics.yamllint,
 }
+
 null_ls.register(null_ls_sources)
