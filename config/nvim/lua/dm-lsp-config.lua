871b26cb5d57f02a56e42b4456ba001184541385
Devin Morgenstern <devin.morgenstern@protonmail.com>
2023-01-18 08:45:55 -0800

    Allow solargraph to run outside of docker

--- config/nvim/lua/dm-lsp-config.lua
+++ config/nvim/lua/dm-lsp-config.lua
@@ -8,6 +8,33 @@ local has_words_before = function()
 end
 local luasnip = require("luasnip")
 
+-- local lsp_formatting = function(bufnr)
+--   vim.lsp.buf.format({
+--     filter = function(client)
+--       -- apply whatever logic you want (in this example, we'll only use null-ls)
+--       return client.name == "null-ls"
+--     end,
+--     bufnr = bufnr,
+--   })
+-- end
+
+-- -- if you want to set up formatting on save, you can use this as a callback
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- -- add to your shared on_attach callback
+-- local on_attach = function(client, bufnr)
+--   if client.supports_method("textDocument/formatting") then
+--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--     vim.api.nvim_create_autocmd("BufWritePre", {
+--       group = augroup,
+--       buffer = bufnr,
+--       callback = function()
+--         lsp_formatting(bufnr)
+--       end,
+--     })
+--   end
+-- end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -136,7 +163,7 @@ lsp.rust_analyzer.setup({
 
 lsp.solargraph.setup({
   capabilities = capabilities,
-  cmd = { 'docker-compose', 'exec', '-T', 'app', 'solargraph', 'stdio' },
+  cmd = { 'solargraph', 'stdio' },
   on_attach = function(client)
     require 'illuminate'.on_attach(client)
     client.server_capabilities.documentFormattingProvider = false
