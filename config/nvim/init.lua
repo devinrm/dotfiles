8104cfc55d73b944f660bac2420117a798a7f080
Devin Morgenstern <devin.morgenstern@protonmail.com>
2023-03-22 15:30:34 -0700

    remove unused plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -295,7 +295,6 @@ require("lazy").setup({
         'https://github.com/rafamadriz/friendly-snippets',
         {
           "https://github.com/zbirenbaum/copilot-cmp",
-          after = { "copilot.lua" },
           dependencies = "https://github.com/zbirenbaum/copilot.lua",
           config = function()
             require("copilot_cmp").setup()
@@ -406,7 +405,6 @@ require("lazy").setup({
             null_ls.builtins.diagnostics.haml_lint,
             null_ls.builtins.diagnostics.jsonlint,
             null_ls.builtins.formatting.prettierd,
-            null_ls.builtins.code_actions.refactoring,
             null_ls.builtins.formatting.rustywind.with({
               extra_filetypes = { "erb" }
             }),
