436a863c549b007697051f3b08391d39fc87c2ca
Devin Morgenstern <devin.morgenstern@protonmail.com>
2023-06-19 15:55:58 -0700

    Add in rubocop

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -348,6 +348,7 @@ require("lazy").setup({
               extra_filetypes = { "erb" }
             }),
             null_ls.builtins.formatting.rubocop,
+            null_ls.builtins.diagnostics.rubocop,
             null_ls.builtins.code_actions.shellcheck,
             null_ls.builtins.diagnostics.shellcheck,
             null_ls.builtins.formatting.shellharden,
