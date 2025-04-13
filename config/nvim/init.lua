a8eb100676184025b4d94722f02978814958247c
Devin Morgenstern <devin.morgenstern@protonmail.com>
2025-04-13 12:00:55 -0700

    fix neotree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -627,6 +627,15 @@ require("lazy").setup({
 			"https://github.com/nvim-neo-tree/neo-tree.nvim",
 			branch = "v3.x",
 			lazy = false,
+			opts = {
+				filesystem = {
+					filtered_items = {
+						visible = true,
+						hide_dotfiles = false,
+						hide_gitignored = true,
+					},
+				},
+			},
 			dependencies = {
 				"https://github.com/nvim-lua/plenary.nvim",
 				"https://github.com/nvim-tree/nvim-web-devicons",
